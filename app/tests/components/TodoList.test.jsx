0b2d4b6eb37826ba3bafa311ff04abd4ddcab144
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-29 15:20:16 +0900

    add test sweet for test components

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,29 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
+
+  it('should render one todo component for each item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check all'
+    }];
+
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  })
+});
