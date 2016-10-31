bbf9db51969066e3de80a2ded9716c0b2635c3b5
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-31 15:57:08 +0900

    add filter fanctionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -7,15 +7,40 @@ module.exports = {
       return todos;
     }
   },
+
   getTodos: function () {
     var stringTodos = localStorage.getItem('todos');
     var todos = [];
     try {
       todos = JSON.parse(stringTodos);
     } catch (e) {
-
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      var lowercaseTodoText = todo.text.toLowerCase();
+      return searchText.length === 0 || lowercaseTodoText.indexOf(searchText) !== -1;
+    });
+
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
