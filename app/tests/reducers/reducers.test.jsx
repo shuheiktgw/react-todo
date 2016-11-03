8ac91f11ef6001c977df9b7e84a9132de46b7ed5
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-11-03 11:51:35 +0900

    add todos reducers and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var reducers = require('reducers');
+var moment = require('moment');
 var df = require('deep-freeze-strict');
 
 describe('Reducers', () => {
@@ -23,6 +24,39 @@ describe('Reducers', () => {
 
       var res = reducers.showCompletedReducer(df(false), df(action));
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var defaultTodos = [{
+        id: 1,
+        text: 'first todo',
+        completed: false,
+        createdAt: moment().unix(),
+        completedAt: undefined
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: defaultTodos[0].id
+      };
+
+      var res = reducers.todosReducer(df(defaultTodos), df(action));
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toExist();
     });
   });
 });
